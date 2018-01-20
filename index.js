60ec76b289ef534b5212faf56eafa037be6f824b
Shaun Tung <shaun.tung@gmail.com>
2018-01-20 05:29:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 const app = "I don't do much."
 
-describe('Arrays', function() {
-  beforeEach(function() {
-    window.kittens = ['Milo', 'Otis', 'Garfield'];
-  });
-
-   describe('destructivelyAppendKitten(name)', function() {
-    it('appends a kitten to the end of the kittens array', function() {
-      destructivelyAppendKitten('Ralph')
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield", "Ralph"])
-    })
-  })
-})
-
 var kittens=['Milo', 'Otis', 'Garfield']
 
 function destructivelyAppendKitten(name){

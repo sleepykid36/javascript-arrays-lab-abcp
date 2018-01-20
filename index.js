ac2cab79bccc9a7ab053992b739d4f22daebe06d
Shaun Tung <shaun.tung@gmail.com>
2018-01-20 05:31:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
+
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
 }
